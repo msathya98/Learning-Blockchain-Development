8a48dd7615c5b830b373500de03d352bbbf3e3ae
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2021-01-07 19:59:38 +0530

    Variables.sol with Uint8, Uint256, bool variables.

---
+++ Variables/variables.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.13;
+
+contract workingWithVariables {
+    uint256 public myUint;
+    
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint * 2;
+    }
+
+
+bool public myBool;
+
+function setMyBool(bool _myBool) public {
+    myBool = !(_myBool);
+}
+
+uint8 public myUint8;
+
+function incrementUint() public {
+    myUint8++;
+    
+}
+
+function decrementUint() public {
+    myUint8--;
+    
+}
+
+
+}
