ece2283b718687f7dbb71c674e5b6195c3d227d2
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2021-01-15 17:46:14 +0530

    simpleWallet smart contract updated with allowance.

--- Solidity-project/SimpleWallet.sol
+++ Solidity-project/SimpleWallet.sol
@@ -6,11 +6,30 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract SimpleWallet is Ownable {
     
-   
-
+    mapping(address => uint) public allowance;
     
-   function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+   
+ 
+   modifier ownerOrAllowed(uint _amount ) {
+       address owner;
+      owner == msg.sender;
       
+       require( ( msg.sender == owner) || allowance[msg.sender] >= _amount, "You are not the owner");
+       _;
+    
+      }
+
+    function reduceAllowance(address _who, uint _amount ) internal {
+        allowance[_who] -= _amount;
+    }
+   function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+      require(_amount <= address(this).balance, "there are not enough funds in smart contract");
+      if(msg.sender != owner) {
+          reduceAllowance(msg.sender, _amount);
+      }
        _to.transfer(_amount);
    }
    
